625b48502aadd81a1387727d840488cc3dedc5e5
Jairo Junior <113320385+jairojrdv@users.noreply.github.com>
2023-04-07 23:33:50 -0300

    Versionando para versão pastor nonato, ajustes a fazer e lógicas a impletar, mas já está funcional para desktop

--- main.js
+++ main.js
@@ -1,89 +1,135 @@
-const mario = document.querySelector('.mario');
+const nonato = document.querySelector('.nonato');
 const pipe = document.querySelector('.pipe');
 const clouds = document.querySelector('.clouds');
+const restartButton = document.querySelector('.restart-button');
+const backGroundSound = document.getElementById('background-music');
+const perdeuSound = document.getElementById('perdeu-music');
 let score = document.querySelector(".score");
 let gameOver = document.querySelector(".game-over");
 let finalScore = document.querySelector(".final-score");
 
 let interval = null;
-let marioScore = 0;
+let nonatoScore = 0;
 let startTime = null;
 let isGameOver = true;
 
 let scoreCounter = () => {
-  const currentTime = new Date().getTime();
-  const elapsedTimeInSeconds = Math.round((currentTime - startTime) / 1000);
-  score.innerHTML = `Score <b>${elapsedTimeInSeconds}</b> seconds`;
+    const currentTime = new Date().getTime();
+    const elapsedTimeInSeconds = Math.round((currentTime - startTime) / 1000);
+    score.innerHTML = `Score <b>${elapsedTimeInSeconds}</b> seconds`;
 }
 
 const jump = () => {
-  if (isGameOver) {
-    return;
-  }
-  
-  mario.classList.add('jump');
+    if (isGameOver) {
+        return;
+    }
 
-  setTimeout(() => {
-    mario.classList.remove('jump');
-  }, 500);
+    
+    nonato.classList.add('jump');
+    nonato.src = './imgs/salto-d.png';
+    nonato.style.width = '140px';
+
+    setTimeout(() => {
+        nonato.classList.remove('jump');
+        nonato.src = './imgs/corre-d.gif';
+    }, 500);
 }
 
 const startGame = () => {
-  pipe.style.animation = 'pipe-animation 2s linear infinite';
-  clouds.style.animation = 'clouds-animation 20s linear infinite';
-  isGameOver = false;
-  marioScore = 0;
-  startTime = new Date().getTime();
-  scoreCounter();
-  interval = setInterval(() => {
-    marioScore++;
+    pipe.style.animation = 'pipe-animation 1.6s linear infinite';
+    clouds.style.animation = 'clouds-animation 20s linear infinite';
+    isGameOver = false;
+    nonatoScore = 0;
+    startTime = new Date().getTime();
     scoreCounter();
-  }, 1000);
+    backGroundSound.play()
+    interval = setInterval(() => {
+        nonatoScore++;
+        scoreCounter();
+    }, 1000);
+
+    // Esconde o conteúdo da classe "start"
+    const startDiv = document.querySelector('.start');
+    startDiv.style.display = 'none';
 }
+
 
 const endGame = () => {
     clearInterval(interval);
     interval = null;
-    isGameOver = true;
-  
+
     // Para fixar a posição do pipe
     const pipePosition = pipe.getBoundingClientRect().left;
     pipe.style.left = `${pipePosition}px`;
-    
+
+    perdeuSound.addEventListener('ended', function () {
+        perdeuSound.pause();
+    });
+    perdeuSound.play();
+    setTimeout(function () {
+        perdeuSound.pause();
+        perdeuSound.currentTime = 0;
+    }, 3000);
+    backGroundSound.pause();
     pipe.style.animation = 'none';
-    mario.style.animation = 'none';
-    mario.src = './imgs/game-over.png';
-    mario.style.width = '75px';
-    mario.style.marginLeft = '50px';
-    finalScore.textContent = `${marioScore} seconds`;
+    nonato.style.animation = 'none';
+    nonato.src = './imgs/perdende.gif';
+    nonato.style.width = '150px';
+    nonato.style.marginLeft = '50px';
+    finalScore.textContent = `${nonatoScore} seconds`;
     gameOver.classList.add("show");
-  }
+}
+
+
 
 const initialize = () => {
-  pipe.style.animation = 'none';
-  clouds.style.animation = 'none';
-  pipe.style.right = '0px';
-  score.innerHTML = `Score <b>0</b> seconds`;
+    pipe.style.animation = 'none';
+    clouds.style.animation = 'none';
+    pipe.style.right = '0px';
+    score.innerHTML = `Score <b>0</b> seconds`;
+
+    // Adiciona event listener para o botão "start" com o mouse
+    const startButton = document.querySelector('.start button');
+    startButton.addEventListener('click', () => {
+        startGame();
+    });
+
+    // Adiciona event listener para o botão "start" em dispositivos móveis
+    startButton.addEventListener('touchstart', () => {
+        startGame();
+    });
 }
 
 initialize();
 
 document.addEventListener('keydown', (event) => {
-  if (event.key === 'ArrowUp' && isGameOver) {
-    startGame();
-  }
-  jump();
+    if (event.key === 'ArrowUp' && isGameOver) {
+        startGame();
+    } else if (event.key === 'ArrowUp') {
+        jump();
+    }
 });
 
-const loop = setInterval(() => {
-  if (isGameOver) {
-    return;
-  }
-  
-  const pipePosition = pipe.offsetLeft;
-  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+document.addEventListener('touchstart', (event) => {
+    if (isGameOver) {
+        startGame();
+    } else {
+        jump();
+    }
+});
 
-  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-    endGame();
-  }
+backGroundSound.play();
+
+
+const loop = setInterval(() => {
+    if (isGameOver) {
+        return;
+    }
+
+    const pipePosition = pipe.offsetLeft;
+    const nonatoPosition = +window.getComputedStyle(nonato).bottom.replace('px', '');
+
+    if (pipePosition <= 120 && pipePosition > 0 && nonatoPosition < 80) {
+        endGame();
+    }
 }, 10);
